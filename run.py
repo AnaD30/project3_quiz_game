030e0e5ebe70c87d10116849f3d2c2697da9ef2a
Ana Duler <ana.duler93@gmail.com>
2024-04-29 20:09:18 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import os
+from rich import rich
+
+def clear_terminal():
+    """
+    Wipes the terminal clean
+    """
+    os.system('cls' if os.name == 'nt' else 'clear')
