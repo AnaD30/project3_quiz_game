ae556e6b95a784563e1b1f714f56f2e79d7b171f
Ana Duler <ana.duler93@gmail.com>
2024-04-29 20:30:51 +0000

    Initial skeleton of the quiz

--- run.py
+++ run.py
@@ -1,11 +1,30 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
 import os
 from rich import rich
 
-def clear_terminal():
-    """
-    Wipes the terminal clean
-    """
-    os.system('cls' if os.name == 'nt' else 'clear')
+def new_quiz():
+    pass
+
+def check_answer():
+    pass
+
+def display_score():
+    pass
+
+def display_score():
+    pass
+
+def play_again():
+    pass
+
+questions = {
+    "What is the  capital city of Croatia?"
+}
+
+options = ["a.Tallin","b.Sarajevo","c.Zagreb"]
+
+answers = ()
+guesses =[]
